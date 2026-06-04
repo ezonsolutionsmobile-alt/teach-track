6d4f235fef2196ca53c490b4116edadb302a7748
ahmaddinkhan03 <ezonsolutionsmobile@gmail.com>
2026-06-04 17:00:50 +0500

    removed unused imports

--- App.jsx
+++ App.jsx
@@ -1,15 +1,11 @@
 import 'react-native-gesture-handler';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Toast from 'react-native-toast-message';
-import { Text } from 'react-native';
 import MainNav from './src/navigation/MainNav';
-import CustomStatusBar from './src/components/CustomStatusBar';
-import themes from './src/themes/colors';
-import { useThemeStore } from "./src/store/useThemeStore";
 import AppBootstrap from './src/bootstrap/AppBootstrap'
 import { toastConfig } from './src/components/ToastConfig';
 import AppWrapperForNetworkCheck from './src/components/AppWrapperForNetworCheck'
